a4049b8ac84d7cc025300dc21c7fd47a4559da6c
Raehoon <raehoon@MacBook-Air.local>
2022-08-17 11:24:00 -0400

    SuppFig6

---
+++ code/analysis/PU1KO_ATAC_analysis.R
@@ -0,0 +1,22 @@
+library(DESeq2)
+library(patchwork)
+
+atac_counts <-  read.table("~/Downloads/rs411_ATAC.readcount.031722.txt", header=TRUE, row.names=1)
+
+atac_counts <- atac_counts[,6:14]
+
+coldata = data.frame(
+  row.names= colnames(atac_counts),
+  condition = c(rep("rs411_Edited", 3), rep("rs411_Het", 3), rep("rs411_WT", 3)))
+
+
+dds <- DESeqDataSetFromMatrix(countData = atac_counts,
+                              colData = coldata,
+                              design = ~ condition)
+
+dds <- DESeq(dds)
+
+
+res_pu1ko <- results(dds,contrast=c("condition","rs411_Edited", "rs411_WT"))
+
+write.table(as.data.frame(res_pu1ko), sep = '\t', file = '../../data/SuppFig6/res_pu1ko.txt', quote=F)
