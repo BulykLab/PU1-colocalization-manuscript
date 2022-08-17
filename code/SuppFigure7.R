8878ca4282733e0a49d0d4fcbf78029e9cfb292d
Raehoon <raehoon@MacBook-Air.local>
2022-08-17 09:04:34 -0400

    Fixed Supp7

--- code/SuppFigure7.R
+++ code/SuppFigure7.R
@@ -9,12 +9,12 @@ source("misc/locuscompare_updated.R")
 
 
 ## Supp - MS GWAS
-ms_2013_file = "../data/SuppFig6/ZC2HC1A_MS_2013.metal.txt"
+ms_2013_file = "../data/SuppFig7/ZC2HC1A_MS_2013.metal.txt"
 gwas_stat = read_metal(ms_2013_file, marker_col = 'rsid', pval_col = 'pval')
 gwas_stat = get_position(gwas_stat)
 
 
-pu1_file = "../data/SuppFig6/PU1_40678.pu1bqtl.metal.txt"
+pu1_file = "../data/SuppFig7/PU1_40678.pu1bqtl.metal.txt"
 pu1_stat = read_metal(pu1_file, marker_col = 'rsid', pval_col = 'pval')
 pu1_stat = get_position(pu1_stat)
 
@@ -56,7 +56,7 @@ p_supp_6a <- ggplot(merged_stat) + geom_point(aes(x=-log10(pval.x), y=-log10(pva
 #                             beta=c(0.11422114409002286, -0.029094),
 #                             se=c(0.018738168792812798, 0.00213564))
 
-rs3808619_effects <- read.csv('../data/SuppFig6/rs3808619_effects.txt', header = T, sep='\t')
+rs3808619_effects <- read.csv('../data/SuppFig7/rs3808619_effects.txt', header = T, sep='\t')
 
 p_supp_6b <- ggplot(rs3808619_effects, aes(y = beta / se, x= source)) +
  geom_col(fill="gray", color="black") +
@@ -70,7 +70,7 @@ p_supp_6b <- ggplot(rs3808619_effects, aes(y = beta / se, x= source)) +
 
 p_supp_6 <- p_supp_6a + p_supp_6b + plot_layout(widths = c(1,0.6))
 
-ggplot2::ggsave('../figures/Supp6.pdf',
+ggplot2::ggsave('../figures/Supp7.pdf',
                plot = p_supp_6,
                device='pdf',
                width=300, height=150, units="mm")
